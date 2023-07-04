57f13ce5f2a9267e618cc50a760ef71d27ca400c
Robert Engel <robert.engel@apollidon.com>
2023-07-04 16:34:10 -0400

    Adding client side data fetch to api for title in ui app.

--- apps/rae-chat-ui/src/app/app.tsx
+++ apps/rae-chat-ui/src/app/app.tsx
@@ -1,15 +1,31 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import styles from './app.module.css';
 
 export function App(): ReactElement {
-    return (
-        <div className={styles.title}>
-            Welcome rae-chat-ui{' '}
-            <span role={'img'} aria-label={'Waving hand emoji'}>
-                👋
-            </span>
-        </div>
-    );
+  const [title, setTitle] = useState<string>('');
+  console.log('our title: ', title);
+  useEffect(() => {
+    const fetchResult = async (): Promise<void> => {
+      const result = await fetch('http://localhost:3333/api', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const json = await result.json();
+
+      setTitle(json.message);
+    };
+    fetchResult();
+  }, []);
+
+  return (
+    <div className={styles.title}>
+      {title}{' '}
+      <span role={'img'} aria-label={'Waving hand emoji'}>
+        👋
+      </span>
+    </div>
+  );
 }
 
 export default App;
