3330359d034a7d8acf7104f9febd4303dfd662cc
Robert Engel <robert.engel@apollidon.com>
2023-07-04 16:35:15 -0400

    Update ui unit test.

--- apps/rae-chat-ui/src/app/app.spec.tsx
+++ apps/rae-chat-ui/src/app/app.spec.tsx
@@ -3,13 +3,13 @@ import { render } from '@testing-library/react';
 import App from './app';
 
 describe('Rae Chat UI', () => {
-    it('should render successfully', () => {
-        const { baseElement } = render(<App />);
-        expect(baseElement).toBeTruthy();
-    });
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toBeTruthy();
+  });
 
-    it('should have a greeting as the title', () => {
-        const { getByText } = render(<App />);
-        expect(getByText(/Welcome rae-chat-ui/i)).toBeTruthy();
-    });
+  it('should match last snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
 });
