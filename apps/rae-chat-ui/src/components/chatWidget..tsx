b76215e30f0a41cb72de3955a280dc729fc15019
Robert Engel <robert.engel@apollidon.com>
2023-07-08 09:33:03 -0400

    Tighten up types in chat widget.

--- apps/rae-chat-ui/src/components/chatWidget..tsx
+++ apps/rae-chat-ui/src/components/chatWidget..tsx
@@ -3,6 +3,8 @@ import { Widget } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 
 const ChatWidget = () => {
+    const title = 'RAE Chat';
+    const subtitle = 'Your friendly-ish chat-bot';
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_, setChatWindowOpen] = useState(true);
     const [userChatMessages, setUserChatMessages] = useState(
@@ -13,11 +15,13 @@ const ChatWidget = () => {
         setChatWindowOpen(!chatWindowOpen);
     };
 
-    const handleNewUserMessage = (message: any) => {
-        console.log(`New message incoming! ${message}`);
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        setUserChatMessages([...userChatMessages, message]);
+    const handleNewUserMessage = (message: string) => {
+        const newUserMessage = message as never;
+
+        setUserChatMessages([
+            ...userChatMessages,
+            newUserMessage,
+        ]);
         // Now send the message throught the backend API
     };
 
@@ -25,8 +29,9 @@ const ChatWidget = () => {
         <Widget
             handleNewUserMessage={handleNewUserMessage}
             handleToggle={handleToggle}
-            title="RAE Chat"
-            subtitle="Your friendlyish chatbot"
+            title={title}
+            subtitle={subtitle}
+            emojis={true}
         />
     );
 };
