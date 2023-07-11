c0ec68072e6dd68458719deb4b3e0902baf65660
Robert Engel <robert.engel@apollidon.com>
2023-07-11 18:56:02 -0400

    Relate apiKeys to useres and setup create key route.

--- apps/rae-chat-users/app/api/auth/keys/route.ts
+++ apps/rae-chat-users/app/api/auth/keys/route.ts
@@ -1,11 +1,26 @@
 import { createApiKeys } from '../../../../lib/apiKeys';
+import { prisma } from '../../../../lib/prisma';
 
-export async function GET(request: Request) {
+export async function GET(req: Request) {
     return new Response('Hello, from API KeyS!');
 }
 
-export async function POST(request: Request) {
+export async function POST(req: Request) {
+    const { email } = await req.json();
     const keyResults = await createApiKeys();
+
+    if (keyResults) {
+        await prisma.userApiKey.create({
+            data: {
+                key: keyResults,
+                keyType: 'API_KEY',
+                user: {
+                    connect: { email },
+                },
+            },
+        });
+    }
+
     return new Response(
         JSON.stringify({ apiKey: keyResults }),
         { headers: { 'Content-Type': 'application/json' } },
