848eac5aa79e1e2a7208f6e6f3280ebfa42b8dbd
Robert Engel <robert.engel@apollidon.com>
2023-07-12 19:05:52 -0400

    Validate given api key against actual user keys in chat users app.

--- apps/rae-chat-users/app/api/auth/keys/route.ts
+++ apps/rae-chat-users/app/api/auth/keys/route.ts
@@ -1,8 +1,22 @@
 import { createApiKeys } from '../../../../lib/apiKeys';
 import { prisma } from '../../../../lib/prisma';
+import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-    return new Response('Hello, from API KeyS!');
+    const { searchParams } = new URL(req.url);
+    const key = searchParams.get('key') || 'no_key_found';
+    const userEmail =
+        searchParams.get('email') || 'no_email_found';
+    const user = await prisma.user.findUnique({
+        where: { email: userEmail },
+        include: { userApiKeys: true },
+    });
+    const userKey = user?.userApiKeys.find(
+        ({ key: userKey }) => userKey === key,
+    );
+    const keyValid = !!userKey;
+
+    return NextResponse.json({ keyValid });
 }
 
 export async function POST(req: Request) {
