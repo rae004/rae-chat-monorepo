e55bb397432ae27c710592593886b9fb3bd79374
Robert Engel <robert.engel@apollidon.com>
2023-07-12 19:28:48 -0400

    Addin route to rae-chat-api that checks api key validity with rae-chat-users service.

--- apps/rae-chat-api/app/lib/checkIfValidKey.ts
+++ apps/rae-chat-api/app/lib/checkIfValidKey.ts
@@ -2,9 +2,18 @@ const checkIfValidKey = async (
     key: string,
     email: string,
 ): Promise<boolean> => {
-    // add route in user service to return if key is valid.
-    console.log('our key: ', key.length);
-    return key.length > 6;
+    const baseUrl =
+        process.env.RAE_CHAT_USERS_API ||
+        'http://localhost:4203';
+    const apiUrl = `${baseUrl}/api/auth/keys?key=${key}&email=${email}`;
+
+    const checkKeyValidityWithServer = await fetch(apiUrl, {
+        method: 'GET',
+    });
+    const { keyValid } =
+        await checkKeyValidityWithServer.json();
+
+    return keyValid;
 };
 
 export default checkIfValidKey;
