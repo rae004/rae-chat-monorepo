74aed9b76ed87a7a131580fc18c6177819838ed4
Robert Engel <robert.engel@apollidon.com>
2023-07-24 18:35:20 -0400

    Starting api route to create widgets.

--- apps/rae-chat-users/middleware.ts
+++ apps/rae-chat-users/middleware.ts
@@ -2,5 +2,7 @@ export { default } from 'next-auth/middleware';
 
 export const config = {
     // matcher: ["/profile"],
-    matcher: ['/((?!api/register|api/auth|api/chat|login).*)'],
+    matcher: [
+        '/((?!api/register|api/auth|api/chat|api/widget|login).*)',
+    ],
 };
